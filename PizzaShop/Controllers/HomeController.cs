3c124fcec06e90834c3e05e8d5fc84bb2154915c
Wei Zhang <wei.zhang4@outlook.com>
2020-04-27 09:31:13 -0700

    Adding comment to HomeController.cs

--- PizzaShop/Controllers/HomeController.cs
+++ PizzaShop/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PizzaShop.Models;
 
+//Test comment - Wei
+
 namespace PizzaShop.Controllers
 {
     public class HomeController : Controller
