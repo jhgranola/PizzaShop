410019f7bff71ccc5567c85f95fa234cdc30b268
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-28 11:12:12 -0700

    Updated Menu Controller

--- PizzaShop/Controllers/MenuController.cs
+++ PizzaShop/Controllers/MenuController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Models;
 
 namespace PizzaShop.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        private IProductRepository repository;
+        public MenuController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index(Product products)
         {
             return View();
         }
