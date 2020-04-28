f4cf853bdebaa90a811a96d4d1254c74b782b881
Daniel Lungaro <chunk_77@hotmail.com>
2020-04-28 11:29:52 -0700

    Updates to repository and DbContext

---
+++ PizzaShop/Models/PizzaShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PizzaShop.Models
+{
+    public class PizzaShopDbContext : DbContext
+    {
+        public PizzaShopDbContext(DbContextOptions<PizzaShopDbContext> options) : base(options)
+        { }
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
