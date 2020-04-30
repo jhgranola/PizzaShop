509b17e0f12a4791d179e78e52fcb4eaafcd3f6e
Caleb Pleasant <calebpleasant@gmail.com>
2020-04-30 09:47:48 -0700

    Added MArkDelivered and MarkCompleted in OrdersController

--- PizzaShop/Controllers/OrderController.cs
+++ PizzaShop/Controllers/OrderController.cs
@@ -4,21 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PizzaShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace PizzaShop.Controllers
 {
     public class OrderController : Controller
     {
-        private IOrderRepository repository;
+        //private IOrderRepository repository;
+        private PizzaShopDbContext context;
 
-        public OrderController(IOrderRepository repoService)
+        public OrderController(PizzaShopDbContext ctx)
         {
-            repository = repoService;
+            context = ctx;
         }
 
         public IActionResult List()
         {
-            return View();
+            return View(context.Orders.Where(o => !o.Completed));
+        }
+
+        [HttpPost]
+        public IActionResult MarkCompleted(int orderID)
+        {
+            Order order = context.Orders.FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Completed = true;
+                context.SaveChanges();
+            }
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpPost]
+        public IActionResult MarkDelivered(int orderID)
+        {
+            Order order = context.Orders.FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Delivered = true;
+                context.SaveChanges();
+            }
+            return RedirectToAction(nameof(List));
         }
 
         public IActionResult Completed()
